1c46c1e8ceb2685c6de887f89a769545349df024
tommyrawle <tommyrawle@hotmail.com>
2018-12-06 16:15:33 +0000

    logs correct lyrics for 1 bottle

---
+++ generateLyrics.js
@@ -0,0 +1,18 @@
+function generateLyrics (num) {
+
+    const bottleSlur = (num <= 50) ? 'sh' : ''
+    const passSlur = (num <= 50) ? 'sh' : ''
+
+    let noBottles = 'No bottlesh of beer on the wall, no bottlesh of beer. Go to the shtore, by some more, 99 bottlesh of beer on the wall. '
+    let oneBottle = '1 bottle of beer on the wall, 1 bottle of beer, you take one down, passh it around, no more bottles of beer on the wall. '
+    let bottles = `${num} bottle${bottleSlur} of beer on the wall, ${num} bottle${bottleSlur} of beer, you take one down, pass${passSlur} it around, ${num-1} bottles ${bottleSlur} of beer on the wall.`
+    
+    if (num === 0) return noBottles
+    if (num === 1) return oneBottle + generateLyrics(num - 1)
+    else return bottles + generateLyrics(num - 1)
+    
+
+   
+
+}
+module.exports = {generateLyrics};
---
+++ spec/generateLyrics.spec.js
@@ -0,0 +1,21 @@
+let expect = require('chai').expect
+let {generateLyrics} = require('../generateLyrics');
+
+describe('generateLyrics', () => {
+    let noBottles = 'No bottlesh of beer on the wall, no bottlesh of beer. Go to the shtore, by some more, 99 bottlesh of beer on the wall. '
+    let oneBottle = '1 bottle of beer on the wall, 1 bottle of beer, you take one down, passh it around, no more bottles of beer on the wall. '
+    // let bottles = `${num} bottle${bottleSlur} of beer on the wall, ${num} bottle${bottleSlur} of beer, you take one down, pass${passSlur} it around, ${num-1} bottles ${bottleSlur} of beer on the wall.`
+    // let num = 2
+    // const bottleSlur = (num <= 50) ? 'sh' : ''
+    // const passSlur = (num <= 50) ? 'sh' : ''
+
+    it('should output one line of the lyric when the number is one', () => {
+        expect(generateLyrics(0)).to.equal(noBottles)
+    });
+    it('should output correct lines for one bottle', () => {
+        expect(generateLyrics(1)).to.equal(oneBottle + noBottles)
+    });
+    it('should output correct lines for one bottle', () => {
+        expect(generateLyrics(2)).to.equal(oneBottle + noBottles)
+    });
+});
